836d39fefcb356e4fecd29348a9390b97f6822a7
AlexSinos <alexissinos@gmail.com>
2024-05-31 10:26:05 +0100

    add buttons for rock paper and scissors

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const startButton = document.getElementById('start-button');
-const script = document.getElementById('script');
 const input = document.createElement("input");
 let computerScore = 0;
 let humanScore = 0;
@@ -8,7 +7,7 @@ let name = '';
 let dialogueArray = [
     '. . . ',
     `You're finally awake!`,
-    `I was getting worried I'd have to call an ambulance or something.`,
+        `I was getting worried I'd have to call an ambulance or something.`,
     `...Anyway, I read your file but I can't seem to remember... what's your name again?`,
     `You can't remember? That's embarrassing! I'll call you Player`,
     `Listen, I don't really care anyway. I'm guessing you've already got an idea of what's going on here, right?`,
@@ -19,11 +18,16 @@ let dialogueArray = [
     `I always find it hilarious how you earthlings think your good deeds will decide your fate in the afterlife.`,
     `I know you devoted your life to saving homeless animals but unfortunately it will have no impact here.`,
     `All it really comes down to is if you can beat me in a game of rock, paper, scissors. Funny right?`,
-    `Best of 5, if you win you go to heaven, if you lose you go to hell`
+    `Best of 5, if you win you go to heaven, if you lose you go to hell`,
+    `So, what will it be, rock, paper or scissors?`
+
 ];
 const laugh = document.querySelector("#laugh");
 const container = document.querySelector(".main");
-const speak = document.querySelector("#speak")
+const speak = document.querySelector("#speak");
+const buttonsContainer = document.createElement("div");
+buttonsContainer.classList.add("buttons");
+
 
 const createChatBox = () => {
     const textBox = document.createElement("div");
@@ -39,23 +43,24 @@ const createContinueButton = () => {
     container.appendChild(continueButton);
     return continueButton;
 };
-
 const advanceDialogue = (textBox, continueButton) => {
     let letter = 0;
-    textBox.textContent = ''; 
+    textBox.textContent = '';
 
     const typeWriter = () => {
         if (letter < dialogueArray[position].length) {
-            speak.play()
+            speak.play();
             textBox.textContent += dialogueArray[position].charAt(letter);
             letter++;
             setTimeout(typeWriter, 30);
         } else {
-            showContinueButton(); 
+            showContinueButton();
         }
     };
 
     const showContinueButton = () => {
+        continueButton.style.display = 'inline-block';
+
         if (position === 6) {
             laugh.play();
             setTimeout(() => {
@@ -64,15 +69,6 @@ const advanceDialogue = (textBox, continueButton) => {
         } else {
             continueButton.style.display = 'inline-block';
         }
-
-        continueButton.onclick = () => {
-            position++;
-            if (position < dialogueArray.length) {
-                advanceDialogue(textBox, continueButton);
-            } else {
-                gameLoop();
-            }
-        };
 
         if (position === 3) {
             continueButton.textContent = "Submit";
@@ -85,13 +81,22 @@ const advanceDialogue = (textBox, continueButton) => {
                 position++;
                 advanceDialogue(textBox, continueButton);
             };
+        } else {
+            continueButton.onclick = () => {
+                position++;
+                if (position < dialogueArray.length) {
+                    advanceDialogue(textBox, continueButton);
+                } else {
+                    continueButton.remove(); 
+                    gameLoop();
+                }
+            };
         }
     };
 
     continueButton.style.display = 'none';
     typeWriter();
 };
-
 
 const startGame = () => {
     const music = document.getElementById("music");
@@ -105,7 +110,6 @@ const startGame = () => {
 
 const checkGameEnd = () => {
     const textBox = document.querySelector(".text-box");
-    const continueButton = document.querySelector("button");
     const death = document.getElementById("death");
     const heaven = document.getElementById("heaven");
     if (computerScore >= 3) {
@@ -115,30 +119,53 @@ const checkGameEnd = () => {
         heaven.play();
         textBox.textContent = `Okay, okay. You win, you can go to heaven with Gucio.`;
     }
-    input.remove();
-    continueButton.remove();
 };
 
 const gameLoop = () => {
     let computerChoice = getComputerChoice();
     const textBox = document.querySelector(".text-box");
-    const continueButton = document.querySelector("button");
-    container.appendChild(input);
-    continueButton.onclick = () => { 
-        let humanChoice = input.value.toLowerCase();
+container.appendChild(buttonsContainer);
+    buttonsContainer.textContent = ''; 
+
+    const rock = document.createElement("button");
+    rock.classList.add('choice-button');
+    rock.textContent = '🪨';
+    buttonsContainer.appendChild(rock);
+
+    const scissors = document.createElement("button");
+    scissors.classList.add('choice-button');
+    scissors.textContent = '✂️';
+    buttonsContainer.appendChild(scissors);
+
+    const paper = document.createElement("button");
+    paper.classList.add('choice-button');
+    paper.textContent = '📄';
+    buttonsContainer.appendChild(paper);
+
+    rock.addEventListener("click", () => {
+        let humanChoice = "rock";
         rockPaperScissors(humanChoice, computerChoice);
-    };
+    });
+
+    scissors.addEventListener("click", () => {
+        let humanChoice = "scissors";
+        rockPaperScissors(humanChoice, computerChoice);
+    });
+
+    paper.addEventListener("click", () => {
+        let humanChoice = "paper";
+        rockPaperScissors(humanChoice, computerChoice);
+    });
 };
 
 const rockPaperScissors = (humanChoice, computerChoice) => {
     const textBox = document.querySelector(".text-box");
-    const continueButton = document.querySelector("button");
     const humanChoiceCapitalised = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
     const rattle = document.querySelector("#rattle");
     rattle.play();
+    document.querySelectorAll('.choice-button').forEach(button => button.remove());
 
-
-    let resultText;
+    let resultText = "";
     if (humanChoice === computerChoice) {
         resultText = `${humanChoiceCapitalised}? I have ${computerChoice}. That's a draw! ${humanScore} : ${computerScore}`;
     } else if (
@@ -153,21 +180,18 @@ const rockPaperScissors = (humanChoice, computerChoice) => {
         (humanChoice === 'paper' && computerChoice === 'rock')
     ) {
         resultText = `${humanChoiceCapitalised}? I have ${computerChoice}. ${++humanScore} : ${computerScore}`;
-    } else {
-        resultText = `What on earth is a ${humanChoice}?`;
     }
 
     let index = 0;
     textBox.textContent = "";
     const typeWriter = () => {
-        speak.play()
         if (index < resultText.length) {
+            speak.play();
             textBox.textContent += resultText.charAt(index);
             index++;
-            setTimeout(typeWriter, 30); 
+            setTimeout(typeWriter, 30);
         } else {
             updateScores();
-
             if (computerScore === 3 || humanScore === 3) {
                 checkGameEnd();
             } else {
@@ -179,21 +203,16 @@ const rockPaperScissors = (humanChoice, computerChoice) => {
     setTimeout(typeWriter, 2000);
 };
 
-
-
 const updateScores = () => {
-    console.log("Updating Scores...");
-    console.log("Previous Scores - Human:", humanScore, "Computer:", computerScore);
     document.querySelector('.computer-score').textContent = computerScore;
     document.querySelector('.human-score').textContent = humanScore;
     document.querySelector('.human-score-title').textContent = name.charAt(0).toUpperCase() + name.slice(1);
     document.querySelector('.computer-score-title').textContent = 'Death';
-    console.log("Updated Scores - Human:", humanScore, "Computer:", computerScore);
 };
 
 function getComputerChoice() {
     let value = Math.random();
-    return (value <= 0.33) ? "scissors" : (value <= 0.66) ? "rock" : "paper"; 
+    return (value <= 0.33) ? "scissors" : (value <= 0.66) ? "rock" : "paper";
 }
 
 startButton.addEventListener('click', () => {
